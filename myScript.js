648275e8e955cfc5c506e35edd15719d2d865400
zhsang <zhsang531@gmail.com>
2017-05-04 00:37:22 +0800

    add myCV. Still some display problems.

--- myScript.js
+++ myScript.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
     $("button").click(function(){
         $("#div1").load("timetable.jpg",function(){
 			$(this).attr("src", "timetable.jpg");
+			$(this).css("position","relatives");
+		});
+		$("#div2").load("myCV.pdf",function(){
+			$(this).attr("src", "myCV.pdf");
+			$(this).css({"height":"400px", "width":"550px"})
 		});
     });
 });
